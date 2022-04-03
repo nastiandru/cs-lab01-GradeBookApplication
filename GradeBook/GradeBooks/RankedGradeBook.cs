57ebf5a2a2a94724c89cdc57e420f6fb92e00c34
nastiandru <80123718+nastiandru@users.noreply.github.com>
2022-04-03 20:58:31 +0300

    Create RankedGradeBook.cs

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,16 @@
+﻿using GradeBook.Enums;
+using System;
+using  System.Linq;
+
+namespace GradeBook.GradeBooks
+{
+	public class RankedGradeBook : BaseGradeBook
+	{
+		public RankedGradeBook(string name,bool isWeighted) : base(name, isWeighted)
+		{
+			Type = GradeBookType.Ranked;
+		}
+
+		
+	}
+}
