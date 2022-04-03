9382f943be1d4e3a729cc7160de67dd4c9908e15
nastiandru <80123718+nastiandru@users.noreply.github.com>
2022-04-03 23:17:21 +0300

    Update RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -41,5 +41,17 @@ namespace GradeBook.GradeBooks
             }
         }
 
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students");
+            }
+            else if (Students.Count > 4)
+            {
+                base.CalculateStudentStatistics(name);
+            }
+        }
+
     }
 }
