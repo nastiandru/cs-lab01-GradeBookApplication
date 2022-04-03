826ce3f02ad2f3cfdccabb132653c5c0adea506f
nastiandru <80123718+nastiandru@users.noreply.github.com>
2022-04-03 23:16:02 +0300

    Update RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -4,13 +4,32 @@ using  System.Linq;
 
 namespace GradeBook.GradeBooks
 {
-	public class RankedGradeBook : BaseGradeBook
-	{
-		public RankedGradeBook(string name,bool isWeighted) : base(name, isWeighted)
-		{
-			Type = GradeBookType.Ranked;
-		}
+    public class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name, bool isWeighted) : base(name, isWeighted)
+        {
+            Type = GradeBookType.Ranked;
+        }
 
-		
-	}
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+                throw new InvalidOperationException();
+            else
+            {
+                if (averageGrade >= 80)
+                    return 'A';
+                else if (averageGrade >= 60)
+                    return 'B';
+                else if (averageGrade >= 40)
+                    return 'C';
+                else if (averageGrade >= 20)
+                    return 'D';
+                else
+                    return 'F';
+            }
+        }
+
+
+    }
 }
